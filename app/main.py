88821f5d1f5cc3e1c1a3526008400dd69cd5e947
BartoszBareja <barejabartosz@gmail.com>
2025-06-14 11:07:35 +0200

    Syncing files

---
+++ app/main.py
@@ -0,0 +1,20 @@
+from fastapi import FastAPI, Request
+from fastapi.responses import HTMLResponse
+from fastapi.staticfiles import StaticFiles
+from fastapi.templating import Jinja2Templates
+
+app = FastAPI()
+
+# Montujemy folder statyczny
+app.mount("/static", StaticFiles(directory="app/static"), name="static")
+
+# Ustawiamy szablony
+templates = Jinja2Templates(directory="app/templates")
+
+@app.get("/", response_class=HTMLResponse)
+async def read_root(request: Request):
+    return templates.TemplateResponse("index.html", {"request": request})
+
+@app.get("/about", response_class=HTMLResponse)
+async def about_page(request: Request):
+    return templates.TemplateResponse("about.html", {"request": request})
